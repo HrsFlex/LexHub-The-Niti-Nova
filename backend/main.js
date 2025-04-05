861cc653fac152897225d82435e2b474e0af4ccc
HrsFlex <harsh42026@gmail.com>
2025-04-06 00:27:54 +0530

    feat: main backend updation

---
+++ backend/main.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
+const { GoogleGenerativeAI } = require("@google/generative-ai");
